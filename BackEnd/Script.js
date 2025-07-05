ea629220cdf0c7a6c2acef7e00f2b41bca2039d7
user.name : umaarkhan <user.email : umaaarkhan2008@gmail.com>
2025-07-06 00:44:49 +0500

    Backend Fixes

--- BackEnd/Script.js
+++ BackEnd/Script.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { Server } = require('socket.io'); 
-const { createServer } = require('http'); 
 const connectDB = require('./Config/DBconnection');
 require('dotenv').config();
 
 connectDB();
 
-const server = createServer(app); 
 
 app.use(cors({
   origin: `${process.env.FRONT_END_URL}`,
@@ -20,6 +17,5 @@ app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 
 app.use("/User", require('./Routes/UserRoutes'));
 
-server.listen(process.env.PORT, () => {});
 
 
