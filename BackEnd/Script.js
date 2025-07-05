d04cb51191860e10cdb2e101b3c9ee61948be159
user.name : umaarkhan <user.email : umaaarkhan2008@gmail.com>
2025-07-05 23:49:44 +0500

    Fix Vercel deploy: rewrites + legacy peer deps

--- BackEnd/Script.js
+++ BackEnd/Script.js
@@ -11,19 +11,15 @@ connectDB();
 const server = createServer(app); 
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: `${process.env.FRONT_END_URL}`,
   credentials: true
 }));
 
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 
-app.get('/test', (req, res) => {
-  res.send("Running");
-});
-
 app.use("/User", require('./Routes/UserRoutes'));
 
-server.listen(5000, () => {});
+server.listen(process.env.PORT, () => {});
 
 
